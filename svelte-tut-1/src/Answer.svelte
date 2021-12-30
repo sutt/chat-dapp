33b870eb2b663923f0a05015fcd7a2c513e87b2f
Will Sutton <wsutton17@gmail.com>
2021-12-30 13:11:43 -0500

    starting up a new learning experience

---
+++ svelte-tut-1/src/Answer.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let answer = "a mystery"
+    export let attribute1
+    export let attribute2
+
+</script>
+<p>The answer is {answer}</p>
+<p>Atty1: {attribute1}</p>
+<p>Atty2: {attribute2}</p>
---
+++ svelte-tut-1/src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Answer from './Answer.svelte'
+import Nested from './Nested.svelte';
+	import MyNested from './Nested.svelte'
+	let name = 'world';
+	let count = 0
+	const src = 'assets/rick.gif'
+	let string = `here's some <strong>HTML</strong>`
+	const myClick = (e) => {count++}
+	$: doubled = count * 2
+	let quadrupled = count * 4
+	// $: if (count > -1) alert("hey")
+	const myObject = {
+		attribute1: "atta-boy-1",
+		attribute2: false
+	}
+	const nums = [13,17,26]
+</script>
+
+<h1>Hello {name.toUpperCase()}</h1>
+<!-- <Answer answer={count}/>
+<Answer {...myObject}/> -->
+<ul class="listicle">
+	{#each nums as num}
+		<li>{num}</li>
+	{/each}
+</ul>
+<!-- <p>string as html</p>
+<p>{@html string}</p> -->
+<p>my-nested:</p>
+<MyNested />
+
+	<button on:click={myClick}>
+		{#if (count < 1)}
+			Click to get started
+		{:else}
+			Click me {count} times
+		{/if}
+	</button>
+
+<p>Doubled: {doubled}</p>
+<p>Quadrupled: {quadrupled} (no $)</p>
+
+<!-- {console.log("yo"); null} -->
+<!-- <img src={src} alt="should worked tho"> -->
+
+<style>
+	h1 {
+		color: purple
+	}
+</style>
